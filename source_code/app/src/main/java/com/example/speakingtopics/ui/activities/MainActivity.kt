1e44e925260d580662c35acd5323dcb23ec7e900
Radoslav Yordanov <yordanov1234@gmail.com>
2022-11-18 10:06:21 +0200

    Add Scaffolding with CustomTopAppBar at MainActivity.kt

--- source_code/app/src/main/java/com/example/speakingtopics/ui/activities/MainActivity.kt
+++ source_code/app/src/main/java/com/example/speakingtopics/ui/activities/MainActivity.kt
@@ -3,18 +3,32 @@ package com.example.speakingtopics.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.speakingtopics.ui.components.CustomTopAppBar
 import com.example.speakingtopics.ui.navigation.NavGraph
 import com.example.speakingtopics.ui.theme.SpeakingTopicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
         setContent {
             SpeakingTopicsTheme {
-                NavGraph()
+
+                val navController = rememberNavController()
+
+                Scaffold(
+                    topBar = {
+                        CustomTopAppBar(navController = navController)
+                    },
+                    content = { NavGraph(navController) }
+                )
             }
         }
     }
