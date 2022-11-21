41ef3a61dece2a53c311dbe5899787733d9a4f5d
Radoslav Yordanov <yordanov1234@gmail.com>
2022-11-21 19:47:23 +0200

    Update NavGraph.kt and navigation Destination.kt according to new arguments for passing

--- source_code/app/src/main/java/com/example/speakingtopics/ui/navigation/Destination.kt
+++ source_code/app/src/main/java/com/example/speakingtopics/ui/navigation/Destination.kt
@@ -1,9 +1,14 @@
 package com.example.speakingtopics.ui.navigation
 
-import com.example.speakingtopics.ui.activities.Greeting
-
 sealed class Destination(val route: String, val title: String) {
     object HomeScreen : Destination("HomeScreen", "Home")
-    object TopicDetailsScreen : Destination("TopicDetailsScreen", "Topic Details")
+
+    object TopicDetailsScreen : Destination(
+        "TopicDetailsScreen/{${Arguments.TOPIC_DETAIL_ID}}",
+        "Topic Details"
+    ) {
+        fun createRoute(topicId: Int) = "TopicDetailsScreen/$topicId"
+    }
+
     object TopicLibraryScreen : Destination("TopicLibraryScreen", "Library")
 }
--- source_code/app/src/main/java/com/example/speakingtopics/ui/navigation/NavGraph.kt
+++ source_code/app/src/main/java/com/example/speakingtopics/ui/navigation/NavGraph.kt
@@ -2,8 +2,10 @@ package com.example.speakingtopics.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.example.speakingtopics.ui.screens.home.HomeScreen
 import com.example.speakingtopics.ui.screens.topicdetails.TopicDetailsScreen
 import com.example.speakingtopics.ui.screens.topiclibrary.TopicLibraryScreen
@@ -18,19 +20,36 @@ fun NavGraph(
     ) {
         composable(Destination.HomeScreen.route) {
             HomeScreen(
-                onNavigateToTopicDetails = {
-                    navController.navigate(Destination.TopicDetailsScreen.route)
+                onNavigateToTopicDetails = { topicId ->
+                    navController.navigate(Destination.TopicDetailsScreen.createRoute(topicId))
                 },
                 onNavigateToLibrary = {
                     navController.navigate((Destination.TopicLibraryScreen.route))
                 }
             )
         }
-        composable(Destination.TopicDetailsScreen.route) {
-            TopicDetailsScreen()
+
+        composable(
+            route = Destination.TopicDetailsScreen.route,
+            arguments = listOf(navArgument(Arguments.TOPIC_DETAIL_ID) {
+                type = NavType.IntType
+            })
+        ) { backStackEntry ->
+            val topicId = backStackEntry.arguments?.getInt(Arguments.TOPIC_DETAIL_ID)
+
+            TopicDetailsScreen(topicId = topicId ?: 0)
         }
+
         composable(Destination.TopicLibraryScreen.route) {
-            TopicLibraryScreen()
+            TopicLibraryScreen(
+                onNavigateToTopicDetails = { topicId ->
+                    navController.navigate(Destination.TopicDetailsScreen.createRoute(topicId))
+                }
+            )
         }
     }
+}
+
+object Arguments {
+    const val TOPIC_DETAIL_ID = "topic_id"
 }
