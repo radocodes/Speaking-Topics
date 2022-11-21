59e7b3075360c0202a4860e3d574d97ae06eb21f
Radoslav Yordanov <yordanov1234@gmail.com>
2022-11-21 19:23:53 +0200

    Add network ApiRoutes

---
+++ source_code/app/src/main/java/com/example/speakingtopics/network/ApiRoutes.kt
@@ -0,0 +1,6 @@
+package com.example.speakingtopics.network
+
+object ApiRoutes {
+    private const val BASE_URL = "https://api.jsonbin.io/v3/qs"
+    const val ALL_DEFAULT_TOPICS = "$BASE_URL/637b2fc70e6a79321e4fc112"
+}
